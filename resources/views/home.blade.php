f9a0c99c3e24e021b53d1a6f7ea411a01fbf2834
Essam Dalal <essamdalal7@gmail.com>
2021-06-10 03:31:15 +0300

    make basics pages

---
+++ resources/views/home.blade.php
@@ -0,0 +1,206 @@
+<x-layouts.app>
+    <section class="hero is-info is-fullheight ">
+        <div class="hero-body">
+            <div class="">
+                <p class="title has-text-centered">
+                    Learning Manegment System
+                </p>
+                <p class="subtitle has-text-centered">
+                    LMS
+                </p>
+            </div>
+        </div>
+    </section>
+
+    <section class="section  pt-6  has-background-success-light">
+        <h1 class="title has-text-centered">Course section</h1>
+        <section class="section ml-5">
+            <div class="columns">
+                <div class="column">
+                    <div class="card">
+                        <div class="card-image">
+                            <figure class="image is-4by3">
+                                <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+                            </figure>
+                        </div>
+                        <div class="card-content">
+                            <div class="media">
+                                <div class="media-left">
+
+                                </div>
+                                <div class="media-content">
+                                    <p class="title is-4">Name Course</p>
+
+                                </div>
+                            </div>
+
+                            <div class="content">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+                                <a href="#">#css</a> <a href="#">#responsive</a>
+                                <br>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="column">
+                    <div class="card">
+                        <div class="card-image">
+                            <figure class="image is-4by3">
+                                <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+                            </figure>
+                        </div>
+                        <div class="card-content">
+                            <div class="media">
+                                <div class="media-left">
+
+                                </div>
+                                <div class="media-content">
+                                    <p class="title is-4">Name Course</p>
+
+                                </div>
+                            </div>
+
+                            <div class="content">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+                                <a href="#">#css</a> <a href="#">#responsive</a>
+                                <br>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="column">
+                    <div class="card">
+                        <div class="card-image">
+                            <figure class="image is-4by3">
+                                <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+                            </figure>
+                        </div>
+                        <div class="card-content">
+                            <div class="media">
+                                <div class="media-left">
+
+                                </div>
+                                <div class="media-content">
+                                    <p class="title is-4">Name Course</p>
+
+                                </div>
+                            </div>
+
+                            <div class="content">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+                                <a href="#">#css</a> <a href="#">#responsive</a>
+                                <br>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <div class="columns ">
+            <div class="column is-half is-offset-one-quarter">
+                <button class="button is-medium is-fullwidth is-primary is-outlined">See ALl</button>
+            </div>
+        </div>
+    </section>
+
+    <section class="section  pt-6  has-background-white-bis">
+        <h1 class="title has-text-centered">Curriculum section</h1>
+        <section class="section ml-5">
+            <div class="columns">
+                <div class="column">
+                    <div class="card">
+                        <div class="card-image">
+                            <figure class="image is-4by3">
+                                <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+                            </figure>
+                        </div>
+                        <div class="card-content">
+                            <div class="media">
+                                <div class="media-left">
+
+                                </div>
+                                <div class="media-content">
+                                    <p class="title is-4">Name Course</p>
+
+                                </div>
+                            </div>
+
+                            <div class="content">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+                                <a href="#">#css</a> <a href="#">#responsive</a>
+                                <br>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="column">
+                    <div class="card">
+                        <div class="card-image">
+                            <figure class="image is-4by3">
+                                <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+                            </figure>
+                        </div>
+                        <div class="card-content">
+                            <div class="media">
+                                <div class="media-left">
+
+                                </div>
+                                <div class="media-content">
+                                    <p class="title is-4">Name Course</p>
+
+                                </div>
+                            </div>
+
+                            <div class="content">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+                                <a href="#">#css</a> <a href="#">#responsive</a>
+                                <br>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="column">
+                    <div class="card">
+                        <div class="card-image">
+                            <figure class="image is-4by3">
+                                <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+                            </figure>
+                        </div>
+                        <div class="card-content">
+                            <div class="media">
+                                <div class="media-left">
+
+                                </div>
+                                <div class="media-content">
+                                    <p class="title is-4">Name Course</p>
+
+                                </div>
+                            </div>
+
+                            <div class="content">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+                                <a href="#">#css</a> <a href="#">#responsive</a>
+                                <br>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <div class="columns ">
+            <div class="column is-half is-offset-one-quarter">
+                <button class="button is-medium is-fullwidth is-primary is-outlined">See ALl</button>
+            </div>
+        </div>
+    </section>
+
+
+</x-layouts.app>
